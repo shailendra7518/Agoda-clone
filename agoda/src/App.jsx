33968777f55dd3ec2eec6e300f06a14258a5d68e
db648 <95957492+db648@users.noreply.github.com>
2022-04-01 15:37:08 +0530

    product left page

--- agoda/src/App.jsx
+++ agoda/src/App.jsx
@@ -2,15 +2,18 @@ import { Footer } from './components/footer/footer'
 import { Login } from './components/login/login'
 import { Register } from './components/login/register'
 import { Navbar } from './components/navbar/navbar'
+import { ProductLeftPage } from './product/leftproductpage'
 
 function App() {
 
   return (
     <div>
       {/* <Navbar /> */}
-      <Login />
+      {/* <Login /> */}
       {/* <Register /> */}
       {/* <Footer /> */}
+
+      <ProductLeftPage />
     </div>
   )
 }
