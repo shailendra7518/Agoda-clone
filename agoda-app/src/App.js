0eea043dd5d8639fb573ba2d7be01c7e2d75e503
Your Name <shailendraftp0001@gmail.com>
2022-04-02 23:32:43 +0530

    added

--- agoda-app/src/App.js
+++ agoda-app/src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Home } from './Components/Home/Home';
 import { TopHidden } from './Components/TopHiddenNav/TopHidden';
+import { FilterPart } from './Components/FIlter/Filter';
 
 function App() {
   return (<>
     <TopHidden/>
+    <FilterPart/>
    <Home/>
    </>
   );
