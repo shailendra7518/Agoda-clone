1dadac4f9635830e41416da5b59ff526bae92144
Ritika <ritikagupta11294@gmail.com>
2022-04-03 13:45:34 +0530

    agoda

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import {Filter} from "./Components/Filter";
+import { Mainbox } from './Components/Mainbox';
+
+function App() {
+  return (
+    <div className="App">
+  
+    <Filter/>
+    <Mainbox/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/RightBox.jsx
@@ -0,0 +1,14 @@
+import { Sort } from "./Sort"
+
+export const RightBox = () => {
+    return (
+        
+        <div>
+            <Sort/>
+            <div style={{border: "1px solid red", width: "95%", height: "80%"}}>
+
+            </div>
+
+        </div>
+    )
+}
